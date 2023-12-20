fc467d022f3074e431c176c22c72ac79c6c7b74c
dash420 <gabrielmialchi@gmail.com>
2023-12-20 19:20:43 -0300

    section 02 class 24
    Selected Unit Visual configuration

--- turnbased-valor/Assets/Scripts/UnitActionSystem.cs
+++ turnbased-valor/Assets/Scripts/UnitActionSystem.cs
@@ -5,17 +5,31 @@ using UnityEngine;
 
 public class UnitActionSystem : MonoBehaviour
 {
+    public static UnitActionSystem Instance { get; private set; } 
+    
     public event EventHandler OnSelectedUnitChanged;
 
     [SerializeField] private Unit selectedUnit;
     [SerializeField] private LayerMask unitLayerMask;
 
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Debug.LogError("There's more than one UnitActionSystem! " + transform + " - " + Instance);
+            Destroy(gameObject);
+            return ;
+        }
+        
+        Instance = this;
+    }
     private void Update() 
     {
 
         if (Input.GetMouseButtonDown(0))
         {
             if (TryHandleUnitSelection()) return;
+
             selectedUnit.Move(MouseWorld.GetPosition());
         }
     }
