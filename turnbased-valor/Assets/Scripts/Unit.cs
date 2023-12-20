3a0481db7ae334cf8a70b30c742500a5d9bf1070
dash420 <gabrielmialchi@gmail.com>
2023-12-20 17:40:53 -0300

    section 02 class 20
    Set Animator Parameters to Animate when Walk / Idle

--- turnbased-valor/Assets/Scripts/Unit.cs
+++ turnbased-valor/Assets/Scripts/Unit.cs
@@ -4,17 +4,29 @@ using UnityEngine;
 
 public class Unit : MonoBehaviour
 {
+    [SerializeField] private Animator unitAnimator;
     private Vector3 targetPosition;
 
     private void Update() 
     {
+        
         float stoppingDistance = .1f;
 
         if (Vector3.Distance(transform.position, targetPosition) > stoppingDistance)
         {
-        Vector3 moveDirection = (targetPosition - transform.position).normalized;
+
         float moveSpeed = 4f;
+        
+        Vector3 moveDirection = (targetPosition - transform.position).normalized;
+        
         transform.position += moveDirection * moveSpeed * Time.deltaTime;
+        
+        unitAnimator.SetBool("isWalking", true);
+        
+        }
+        else 
+        {
+        unitAnimator.SetBool("isWalking", false);
         }
         
 
