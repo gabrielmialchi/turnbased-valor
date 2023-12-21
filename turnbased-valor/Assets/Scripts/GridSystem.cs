32c0d685d06d17ed69dc416f456aee0d6e415fec
dash420 <gabrielmialchi@gmail.com>
2023-12-21 15:37:47 -0300

    section 03 class 28
    Create Grid Game Object

--- turnbased-valor/Assets/Scripts/GridSystem.cs
+++ turnbased-valor/Assets/Scripts/GridSystem.cs
@@ -7,6 +7,7 @@ public class GridSystem
     private int width;
     private int height;
     private float cellSize;
+    private GridObject[,] gridObjectArray; 
 
     public GridSystem(int width, int height, float cellSize)
     {
@@ -14,11 +15,15 @@ public class GridSystem
         this.height = height;
         this.cellSize = cellSize;
 
+        gridObjectArray = new GridObject[width, height];
+
         for(int x = 0; x < width; x++)
         {
             for (int z = 0; z < height; z++)
             {
-                Debug.DrawLine(GetWorldPosition(x, z), GetWorldPosition(x, z) + Vector3.right * .2f, Color.white, 1000);
+
+                GridPosition gridPosition = new GridPosition(x, z);
+                gridObjectArray[x, z] = new GridObject(this, gridPosition);
 
             }
         }
@@ -36,6 +41,19 @@ public class GridSystem
             Mathf.RoundToInt(worldPosition.x / cellSize), 
             Mathf.RoundToInt(worldPosition.z / cellSize)
             );
+    }
+
+    public void CreateDebugObjects(Transform debugPrefab)
+    {
+        for (int x = 0; x < width; x++)
+        {
+            for (int z = 0; z < height; z++)
+            {
+
+                GameObject.Instantiate(debugPrefab, GetWorldPosition(x, z), Quaternion.identity);
+
+            }
+        }
     }
 
 }
