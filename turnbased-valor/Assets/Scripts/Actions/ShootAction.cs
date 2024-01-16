26c9b0bfb896fce2b88240feb130d42991cbba32
dash420 <gabrielmialchi@gmail.com>
2024-01-15 22:56:05 -0300

    section 05 class 52
    implementing Shoot Action logic, visuals and VFX

--- turnbased-valor/Assets/Scripts/Actions/ShootAction.cs
+++ turnbased-valor/Assets/Scripts/Actions/ShootAction.cs
@@ -6,7 +6,13 @@ using UnityEngine.EventSystems;
 
 public class ShootAction : BaseAction
 {
-    public event EventHandler OnShoot;
+    public event EventHandler<OnShootEventArgs> OnShoot;
+
+    public class OnShootEventArgs : EventArgs
+    {
+        public Unit targetUnit;
+        public Unit shootingUnit;
+    }
 
     private enum State
     {
@@ -76,7 +82,12 @@ public class ShootAction : BaseAction
 
     private void Shoot()
     {
-        OnShoot?.Invoke(this, EventArgs.Empty);
+        OnShoot?.Invoke(this, new OnShootEventArgs
+        {
+            targetUnit = targetUnit,
+            shootingUnit = unit
+        });
+
         targetUnit.Damage();
     }
 
--- turnbased-valor/Assets/Scripts/UnitAnimator.cs
+++ turnbased-valor/Assets/Scripts/UnitAnimator.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 public class UnitAnimator : MonoBehaviour
 {
     [SerializeField] private Animator animator;
+    [SerializeField] private Transform bulletProjectilePrefab;
+    [SerializeField] private Transform shootPointTransform;
 
     private void Awake()
     {
@@ -21,11 +23,6 @@ public class UnitAnimator : MonoBehaviour
         }
     }
 
-    private void ShootAction_OnShoot(object sender, EventArgs e)
-    {
-        animator.SetTrigger("Shoot");
-    }
-
     private void MoveAction_OnStartMoving(object sender, EventArgs e)
     {
         animator.SetBool("isWalking", true);
@@ -34,5 +31,21 @@ public class UnitAnimator : MonoBehaviour
     private void MoveAction_OnStopMoving(object sender, EventArgs e)
     {
         animator.SetBool("isWalking", false);
+    }
+
+    private void ShootAction_OnShoot(object sender, ShootAction.OnShootEventArgs e)
+    {
+        animator.SetTrigger("Shoot");
+
+        Transform bulletProjectileTransform = 
+            Instantiate(bulletProjectilePrefab, shootPointTransform.position, Quaternion.identity);
+
+        BulletProjectile bulletProjectile  = bulletProjectileTransform.GetComponent<BulletProjectile>();
+
+        Vector3 targetUnitShootAtPosition = e.targetUnit.GetWorldPosition();
+
+        targetUnitShootAtPosition.y = shootPointTransform.position.y;
+
+        bulletProjectile.Setup(targetUnitShootAtPosition);
     }
 }
