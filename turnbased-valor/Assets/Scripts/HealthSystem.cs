dd9091f60c8e6a4fc99ca1ccb97c9161331947ae
dash420 <gabrielmialchi@gmail.com>
2024-01-24 17:30:02 -0300

    section 05 class 55
    Unit World UI (health bar and action points) implemented

--- turnbased-valor/Assets/Scripts/HealthSystem.cs
+++ turnbased-valor/Assets/Scripts/HealthSystem.cs
@@ -7,8 +7,16 @@ public class HealthSystem : MonoBehaviour
 {
 
     public event EventHandler OnDead;
+    public event EventHandler OnDamaged;
     
     [SerializeField] private int health = 100;
+
+    private int healthMax;
+
+    private void Awake()
+    {
+        healthMax = health;
+    }
 
     public void Damage(int damageAmount)
     {
@@ -18,6 +26,8 @@ public class HealthSystem : MonoBehaviour
         {
             health = 0;
         }
+
+        OnDamaged?.Invoke(this, EventArgs.Empty);
 
         if (health == 0)
         {
@@ -30,5 +40,10 @@ public class HealthSystem : MonoBehaviour
     private void Die()
     {
         OnDead?.Invoke(this, EventArgs.Empty);
+    }
+
+    public float GetHealthNormalized()
+    {
+        return (float)health / healthMax;
     }
 }
