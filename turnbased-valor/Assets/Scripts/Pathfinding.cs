f394eaa0c4c7a8834c0544506fd064fdb5f40a00
dash420 <gabrielmialchi@gmail.com>
2024-02-04 21:17:20 -0300

    section 06 class 65
    part I - Just started the class but already stopped

--- turnbased-valor/Assets/Scripts/Pathfinding.cs
+++ turnbased-valor/Assets/Scripts/Pathfinding.cs
@@ -19,4 +19,26 @@ public class Pathfinding : MonoBehaviour
         gridSystem.CreateDebugObjects(gridDebugObjectPrefab);
 
     }
+
+    public List<GridPosition> FindPath(GridPosition startGridPosition, GridPosition endGridPosition)
+    {
+        List<PathNode> openList = new List<PathNode>();
+        List<PathNode> closedList = new List<PathNode>();
+
+        PathNode startNode = gridSystem.GetGridObject(startGridPosition);
+        openList.Add(startNode);
+
+        for (int x = 0; x < gridSystem.GetWidth(); x++)
+        {
+            for (int z = 0; z < gridSystem.GetHeight(); z++)
+            {
+                GridPosition gridPosition = new GridPosition(x, z);
+                PathNode pathNode = gridSystem.GetGridObject(gridPosition);
+
+                //parei aqui, aula 65
+            }
+        }
+
+    }
+
 }
