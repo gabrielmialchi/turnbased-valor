d706253fbe0911daa316ccc058f3f8bd68c92ba9
dash420 <gabrielmialchi@gmail.com>
2024-02-05 18:40:05 -0300

    section 06 class 65 II
    part II - Pathfing Algorithm implementation

--- turnbased-valor/Assets/Scripts/Grid/GridPosition.cs
+++ turnbased-valor/Assets/Scripts/Grid/GridPosition.cs
@@ -37,19 +37,24 @@ public struct GridPosition : IEquatable<GridPosition>
         return $"x: {x}; z: {z}";
     }
 
-    public static bool operator ==(GridPosition a, GridPosition b)
+    public static bool operator == (GridPosition a, GridPosition b)
     {
         return a.x == b.x && a.z == b.z;
     }
 
-    public static bool operator !=(GridPosition a, GridPosition b)
+    public static bool operator != (GridPosition a, GridPosition b)
     {
         return !(a == b);
     }
 
-    public static GridPosition operator +(GridPosition a, GridPosition b)
+    public static GridPosition operator + (GridPosition a, GridPosition b)
     {
         return new GridPosition(a.x + b.x, a.z + b.z);
+    }
+
+    public static GridPosition operator - (GridPosition a, GridPosition b)
+    {
+        return new GridPosition(a.x - b.x, a.z - b.z);
     }
 
 }
